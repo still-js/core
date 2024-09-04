d28a04d1116056baaf1ef8e713daf075b454c9f6
Nakassony Bernardo <nakassony.bernardo@tistech.co.ao>
2024-09-05 00:28:07 +0100

    adjustement on Menu component and transforming to class

---
+++ app/components/main-menu/Menu.js
@@ -0,0 +1,13 @@
+
+class Menu extends BaseComponent {
+
+    constructor({ componentName, path }){
+        super();
+        this.setup({
+            componentName, 
+            path,
+            imports: []
+        });
+    }
+
+}
--- app/components/main-menu/menu.js
+++
@@ -1,6 +0,0 @@
-function menu(){
-
-    alert(`Validate dynamic code loaded call`);
-    return `This component was loaded dynamically`;
-
-}

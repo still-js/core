c975c7b4e8f7c755a51eec0a8d587dce916aa501
Nakassony Bernardo <nakassony.bernardo@tistech.co.ao>
2024-09-05 00:26:17 +0100

    added component mapping file

---
+++ app/components/component-map.js
@@ -0,0 +1,14 @@
+const UIComponents = {
+    // Component Map setup where 
+    // Key (left side) is hte componente name and
+    // Value (right side) is the component path
+    //
+    //  Component       Component
+    //    Name             Path
+    //     |                 |
+    //     |                 |
+    //     V                 V
+    map: {
+        'menu': 'components/main-menu',
+    }
+};

87e8be27659b1eb2db0275e242bae406c595a4ee
root <root@DELL-CETIM>
2024-10-07 22:16:46 +0100

    feat: list processos

--- app/components/processos/ProcessosGrid.js
+++ app/components/processos/ProcessosGrid.js
@@ -7,8 +7,8 @@ class ProcessosGrid extends ViewComponent {
         <div class="row">
             <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
                 <div class="title-grid-component">
-                    <span class="fas fa-user-friends title-grid-component-icon"></span>    
-                        <h3>Lista dos Colaboradores</h3>
+                    <span class="fas fa-folder title-grid-component-icon"></span>    
+                        <h3>Lista dos Processos</h3>
                     <span class="title-grid-component-description">Navega pela lista ou cria um novo</span>
                 </div>
             </div>
@@ -16,7 +16,7 @@ class ProcessosGrid extends ViewComponent {
         <div class="row clearfix">
             <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
                 <div>
-                    <button (click)="gotoView('ColaboradorForm')" type="button" class="btn btn-primary m-t-15 waves-effect">Novo</button>
+                    <button (click)="gotoView('ProcessoForm')" type="button" class="btn btn-primary m-t-15 waves-effect">Novo</button>
                 </div>
                 <div class="card">
                     <div class="header" 
@@ -35,24 +35,39 @@ class ProcessosGrid extends ViewComponent {
                             <table class="table table-bordered table-striped table-hover js-basic-example dataTable">
                                 <thead>
                                     <tr>
-                                        <th width="300px">Nome</th>
-                                        <th>Tipo Colabordor</th>
-                                        <th>Função</th>
-                                        <th>Status</th>
+                                        <th width="100px">Estado</th>
+                                        <th>Referência</th>
+                                        <th>Assunto</th>
+                                        <th>Área</th>
+                                        <th>Fase</th>
+                                        <th>Instituição</th>
+                                        <th>Modo Facturação</th>
+                                        <th>Gestor</th>
+                                        <th>Data Emissão</th>
                                         <th>Acções</th>
                                     </tr>
                                 </thead>
                                 <!-- <output of="dataSource"> -->
                                 <tbody (forEach)="dataSource">
                                     <tr each="item">
-                                        <td>{item.nomeCompleto}</td>
-                                        <td>{item.tipoColaborador}</td>
-                                        <td>{item.funcao}</td>
-                                        <td>{item.status}</td>
+                                        <td>{item.estado}</td>
+                                        <td>{item.ref}</td>
+                                        <td>{item.assunto}</td>
+                                        <td>{item.area}</td>
+                                        <td>{item.fase}</td>
+                                        <td>{item.instituicao}</td>
+                                        <td>{item.modo_facturacao}</td>
+                                        <td>{item.gestor}</td>
+                                        <td>{item.data_registo}</td>
                                         <td class="center" >
-                                           <a title="Editar Colaborador" style="cursor: pointer" (click)="editColaborador('{item.idColaborador}')">
-                                                <span class="fas fa-pencil-alt"></span>
-                                           </a>
+                                          <div style="display: flex; gap: 20px; justify-content: center;">
+                                            <a title="Editar Processo" style="cursor: pointer" (click)="editProcesso('{item.id}')">
+                                              <span class="fas fa-pencil-alt"></span>
+                                            </a>
+                                            <a title="Ver detalhes do Processo" style="cursor: pointer" (click)="detalhesProcesso('{item.id}')">
+                                              <span class="fas fa-file-alt"></span>
+                                            </a>
+                                          </div>
                                         </td>
                                     </tr>
                                 </tbody>                              
@@ -70,6 +85,12 @@ class ProcessosGrid extends ViewComponent {
     this.setup({});
   }
 
+  
+  gotoView(viewComponent) {
+    Router.goto(viewComponent);
+  }
+
+
   async onRender() {
     /**
      * Isso quer dizer que o import do JQuery foi feito no index principal
@@ -85,18 +106,19 @@ class ProcessosGrid extends ViewComponent {
     await this.stLazyExecution(async () => {});
   }
 
- /*
   stAfterInit(val) {
-    $still.HTTPClient.get("http://localhost:3000/api/v1/colaborador/").then(
+    $still.HTTPClient.get("http://localhost:3000/api/v1/processo/").then(
       (r) => {
         if (r.data) {
           console.log(r);
-          this.makeColaboradorDTO(r.data);
+          this.dataSource = r.data
+          // this.makeColaboradorDTO(r.data);
         }
       }
-    );
-  }
-
+      );
+    }
+    
+  /*
   editClient(nif) {
     console.log(`Clicked client is: `, nif);
 

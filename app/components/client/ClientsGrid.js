64c655fc3d279970221a942b96a850441c6c58c5
Nakassony Bernardo <nakassony.bernardo@tistech.co.ao>
2024-10-18 04:20:25 +0100

    clean up on the client form and migrating data table to tabulator component

--- app/components/client/ClientsGrid.js
+++ app/components/client/ClientsGrid.js
@@ -4,107 +4,26 @@ class ClientsGrid extends ViewComponent {
     dataSource;
     /** @type { TabulatorComponent } */
     dataTable = Proxy;
-    /* fields = JSON.stringify([
-        { "title": "Tipo Cliente", "field": "tipoCliente", "sorter": "string", "width": 200 },
-        { "title": "Nome", "field": "nome", "sorter": "string", "width": 200 },
-        { "title": "NIF", "field": "name", "sorter": "string", "width": 200 },
-        { "title": "Endereco", "field": "name", "sorter": "string", "width": 200 },
-        { "title": "Telefone", "field": "name", "sorter": "string", "width": 200 },
-        { "title": "Telefone Cobrança", "field": "name", "sorter": "string", "width": 200 },
-    ]); */
+    dataTableLabels = Prop(JSON.stringify(            [
+        { title: "Tipo Cliente", field: "tipo_id", sorter: "string", width: 200 },
+        { title: "Nome", field: "denominacao", sorter: "string"  },
+        { title: "NIF", field: "nif", sorter: "string" },
+        { title: "Endereco", field: "endereco", sorter: "string" },
+        { title: "Telefone", field: "pessoa_contacto", sorter: "string" },
+        { title: "Telefone Cobrança", field: "contacto_cobranca", sorter: "string" }
+    ]));
 
     template = `
     <section class="content">
 
-        <st-extern
-            component="tabulator-datatable"
+        <st-element
+            component="TabulatorComponent"
             proxy="dataTable"
-            fields='
-            [
-                { "title": "Tipo Cliente", "field": "tipo_id", "sorter": "string", "width": 200 },
-                { "title": "Nome", "field": "denominacao", "sorter": "string"  },
-                { "title": "NIF", "field": "nif", "sorter": "string" },
-                { "title": "Endereco", "field": "endereco", "sorter": "string" },
-                { "title": "Telefone", "field": "pessoa_contacto", "sorter": "string" },
-                { "title": "Telefone Cobrança", "field": "contacto_cobranca", "sorter": "string" }
-            ]
-            '
+            tableHeader="parent.dataTableLabels"
+            (onCellClick)="getClientDetails(fieldName, data)"
             >
-        </st-extern>
+        </st-element>
         
-        <div class="row clearfix">
-            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-                <button (click)="runLocalFunc()">Click here Button</button>
-                <div class="card">
-                    <div class="header">
-                        <h2>
-                            <strong>Clientes</strong> registados</h2>
-                        <ul class="header-dropdown m-r--5">
-                            <li class="dropdown">
-                                <a href="#" onClick="return false;" class="dropdown-toggle" data-toggle="dropdown"
-                                    role="button" aria-haspopup="true" aria-expanded="false">
-                                    <i class="material-icons">more_vert</i>
-                                </a>
-                                <ul class="dropdown-menu pull-right">
-                                    <li>
-                                        <a href="#" onClick="return false;">Action</a>
-                                    </li>
-                                    <li>
-                                        <a href="#" onClick="return false;">Another action</a>
-                                    </li>
-                                    <li>
-                                        <a href="#" onClick="return false;">Something else here</a>
-                                    </li>
-                                </ul>
-                            </li>
-                        </ul>
-                    </div>
-                    <div class="body">
-                        <div class="table-responsive">
-                            <table class="table table-bordered table-striped table-hover js-basic-example dataTable">
-                                <thead>
-                                    <tr>
-                                        <th>Tipo cliente</th>
-                                        <th>Nome</th>
-                                        <th>NIF</th>
-                                        <th>Endereço</th>
-                                        <th>Telefone</th>
-                                        <th>Telefone Cobrança</th>
-                                        <th>&nbsp;</th>
-                                    </tr>
-                                </thead>
-                                <!-- <output of="dataSource"> -->
-                                <tbody (forEach)="dataSource">
-                                    <tr each="item">
-                                        <td>{item.tipo_id}</td>
-                                        <td>{item.denominacao}</td>
-                                        <td>{item.nif}</td>
-                                        <td>{item.endereco}</td>
-                                        <td>{item.pessoa_contacto}</td>
-                                        <td>{item.contacto_cobranca}</td>
-                                        <td>
-                                           <a (click)="editClient('{item.nif}')">Editar</a>
-                                        </td>
-                                    </tr>
-                                </tbody>
-
-                                <tfoot>
-                                <tr>
-                                    <th>Tipo cliente</th>
-                                    <th>Nome</th>
-                                    <th>NIF</th>
-                                    <th>Endereço</th>
-                                    <th>Telefone</th>
-                                    <th>Telefone Cobrança</th>
-                                    <th>&nbsp;</th>
-                                </tr>
-                        </tfoot>
-                            </table>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
     </section>
     `;
 
@@ -154,30 +73,51 @@ class ClientsGrid extends ViewComponent {
 
     }
 
-    editClient(nif){
-        console.log(`Clicked client is: `,nif);
-        
-        const result = this.dataSource.value.filter((r) => r.nif == nif)
-        Router.goto('ClientForm', {
-            data: result[0]
-        });
-        
-    }
-
     /** For Test purpose only */
     /** @type { StEvent } */
     anyState = 'This is the state value';
     runLocalFunc(){
-
-        this.dataTable.clearTable();
-
         this.dataTable.dataSource = [
-            { id: 2, name: "Mary May", gender: "female", rating: 2, col: "blue" }
+            { denominacao: 'Novo valor', tipo_id: 190 }
         ];
+    }
 
-        //console.log(this.dataTable);
+    getClientDetails(_, row){
 
-        alert('Alert from the components itself'+this.anyState);
+        const {
+            contacto_cobranca,
+            created_at,
+            denominacao,
+            endereco,
+            id,
+            nif,
+            nota,
+            pessoa_contacto,
+            status,
+            tipo,
+            tipo_id,
+            updated_at,
+        } = row;
+
+        const data = {
+            contacto_cobranca,
+            created_at,
+            denominacao,
+            endereco,
+            id,
+            nif,
+            nota,
+            pessoa_contacto,
+            status,
+            tipo,
+            tipo_id,
+            updated_at,
+        }
+            
+        Router.goto('ClientForm', {
+            data
+        });
+
     }
     
 

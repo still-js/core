3c8e46775280c5beda9c3cd16f6b11f5ae7951b4
Nakassony Bernardo <nakassony.bernardo@tistech.co.ao>
2025-03-05 03:17:58 +0100

    adjust the app-setup to setup the home component as default

--- app-setup.js
+++ app-setup.js
@@ -1,5 +1,6 @@
 import { StillAppMixin } from "./@still/component/super/AppMixin.js";
 import { Components } from "./@still/setup/components.js";
+import { AppTemplate } from "./app-template.js";
 import { HomeComponent } from "./app/home/HomeComponent.js";
 
 export class StillAppSetup extends StillAppMixin(Components) {
@@ -7,10 +8,11 @@ export class StillAppSetup extends StillAppMixin(Components) {
 
     constructor() {
         super();
+        this.setHomeComponent(HomeComponent);
     }
 
     init() {
-        return new HomeComponent();
+        return new AppTemplate();
     }
 
 }

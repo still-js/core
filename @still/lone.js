7540ac20aeb7866e2114ca3bcec4b4e29c6d1f08
Nakassony Bernardo <nakassony.bernardo@tistech.co.ao>
2025-03-28 05:45:22 +0100

    adding Lone Component capabilities to allow using Still.js component straight on the HTML as well as implement microfrontend

---
+++ @still/lone.js
@@ -0,0 +1,58 @@
+import { StillAppSetup } from "../app-setup.js";
+import { stillRoutesMap } from "../route.map.js";
+import { ComponentNotFoundException, ComponentRegistror } from "./component/manager/registror.js";
+import { BaseComponent } from "./component/super/BaseComponent.js";
+import { BehaviorComponent } from "./component/super/BehaviorComponent.js";
+import { Router } from "./routing/router.js";
+import { Components } from "./setup/components.js";
+import { UUIDUtil } from "./util/UUIDUtil.js";
+
+
+(() => {
+
+    Router.parseRouteMap()
+        .then(async () => {
+
+            Router.setStillHomeUrl();
+            Components.loadCssAssets();
+            StillAppSetup.loadInterceptWorker();
+
+            StillAppSetup.register(Router);
+            StillAppSetup.register(stillRoutesMap);
+            StillAppSetup.register(UUIDUtil);
+            StillAppSetup.register(BehaviorComponent);
+            StillAppSetup.register(Components)
+
+            StillAppSetup.register(ComponentNotFoundException);
+            /** Only for dev mode */ StillAppSetup.setDevErrorTracing();
+
+            document.addEventListener('DOMContentLoaded', () => {
+
+                const stillDeclarations = document.getElementsByTagName('st-element');
+
+                [...stillDeclarations].forEach(async elm => {
+
+                    elm.id = `stillLone_${UUIDUtil.newId()}`
+                    const cmpName = elm.getAttribute('component');
+                    const { newInstance: cmp } = await Components.produceComponent({ cmp: cmpName, lone: true });
+                    new BaseComponent().parseStTag(elm.outerHTML, '', cmp);
+                    const template = cmp.getBoundTemplate(elm.id);
+
+                    ComponentRegistror.add(cmp.cmpInternalId, cmp);
+                    setTimeout(() => cmp.parseOnChange(), 500);
+                    setTimeout(() => (new Components).parseGetsAndSets(cmp), 10);
+                    document.getElementById(elm.id).innerHTML = template;
+
+
+                    const cmpParts = Components.componentPartsMap[cmp.cmpInternalId];
+                    setTimeout(() =>
+                        Components.handleInPartsImpl(cmp, cmp.cmpInternalId, cmpParts)
+                    );
+
+                });
+
+            });
+
+        });
+
+})()

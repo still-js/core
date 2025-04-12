4d224a82bd8ae8c01e1ad9c0cc838aa2d8bedb09
Nakassony Bernardo <nakassony.bernardo@tistech.co.ao>
2025-04-12 20:09:06 +0100

    exposing service event

--- @still/lone.js
+++ @still/lone.js
@@ -3,7 +3,7 @@ import { stillRoutesMap } from "../route.map.js";
 import { ComponentNotFoundException, ComponentRegistror } from "./component/manager/registror.js";
 import { BaseComponent } from "./component/super/BaseComponent.js";
 import { BehaviorComponent } from "./component/super/BehaviorComponent.js";
-import { BaseService } from "./component/super/service/BaseService.js";
+import { BaseService, ServiceEvent } from "./component/super/service/BaseService.js";
 import { Router } from "./routing/router.js";
 import { Components } from "./setup/components.js";
 import { UUIDUtil } from "./util/UUIDUtil.js";
@@ -24,6 +24,7 @@ import { UUIDUtil } from "./util/UUIDUtil.js";
             StillAppSetup.register(BehaviorComponent);
             StillAppSetup.register(Components);
             StillAppSetup.register(BaseService);
+            StillAppSetup.register(ServiceEvent);
 
             StillAppSetup.register(ComponentNotFoundException);
             /** Only for dev mode */ StillAppSetup.setDevErrorTracing();

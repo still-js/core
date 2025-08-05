a51d301b35e05278d13b57c7f06dad0bb712b741
Nakassony Bernardo <sonybernardo_10@hotmail.com>
2025-08-05 00:10:13 -0400

    change package version and fixed Load worker import/setup

--- @still/setup/components.js
+++ @still/setup/components.js
@@ -1624,7 +1624,7 @@ export class Components {
     static loadLoadtWorker() {
         if ('serviceWorker' in navigator) {
             let baseUrl = Components.obj().parseBaseUrl(Router.baseUrl);
-            if(!STILL_HOME_PREXIF) baseUrl = 'https://cdn.jsdelivr.net/npm/@stilljs/core@latest/';
+            if(STILL_HOME_PREXIF) baseUrl = 'https://cdn.jsdelivr.net/npm/@stilljs/core@latest/';
             StillAppSetup.get().loadWorker = new Worker(`${baseUrl}@still/component/manager/load_worker.js`, { type: 'module' });
         }
     }
